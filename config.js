b27f71afa5b87d5ef5e19fbbf1778418d3d18f11
Basti Gonzales <bastigonzales@protonmail.com>
2023-06-06 20:05:54 +0800

    replaced twitter shortcut to lastfm.

--- config.js
+++ config.js
@@ -11,29 +11,29 @@ const CONFIG = {
 	// └─┘┴ ┴└─┘┴└─┘└─┘
 
 	// General
-	name: 'Sebastian',
+	name: "Sebastian",
 	imageBackground: true,
 	openInNewTab: true,
 	twelveHourFormat: true,
 
 	// Greetings
-	greetingMorning: 'Good morning,',
-	greetingAfternoon: 'Good afternoon 🌞, ',
-	greetingEvening: 'Good evening 🌚 ,',
-	greetingNight: '𝓈𝒾𝒸 𝓅𝒶𝓇𝓋𝒾𝓈 𝓂𝒶ℊ𝓃𝒶,',
+	greetingMorning: "Good morning,",
+	greetingAfternoon: "Good afternoon 🌞, ",
+	greetingEvening: "Good evening 🌚 ,",
+	greetingNight: "𝓈𝒾𝒸 𝓅𝒶𝓇𝓋𝒾𝓈 𝓂𝒶ℊ𝓃𝒶,",
 
 	// Layout
-	bentoLayout: 'lists', // 'bento', 'lists', 'buttons'
+	bentoLayout: "lists", // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: '884f47355ccbd22c7ee1db8e3d3eaed5', // Write here your API Key
-	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
-	weatherUnit: 'C', // 'F', 'C'
-	language: 'en', // More languages in https://openweathermap.org/current#multi
+	weatherKey: "884f47355ccbd22c7ee1db8e3d3eaed5", // Write here your API Key
+	weatherIcons: "OneDark", // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherUnit: "C", // 'F', 'C'
+	language: "en", // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '14.647740',
-	defaultLongitude: '121.017464',
+	defaultLatitude: "14.647740",
+	defaultLongitude: "121.017464",
 
 	// Autochange
 	autoChangeTheme: true,
@@ -43,8 +43,8 @@ const CONFIG = {
 
 	// Autochange by hour options (24hrs format, string must be in: hh:mm)
 	changeThemeByHour: false,
-	hourDarkThemeActive: '18:30',
-	hourDarkThemeInactive: '07:00',
+	hourDarkThemeActive: "18:30",
+	hourDarkThemeInactive: "07:00",
 
 	// ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
 	// ├┴┐│ │ │  │ │ ││││└─┐
@@ -52,79 +52,79 @@ const CONFIG = {
 
 	firstButtonsContainer: [
 		{
-			id: '1',
-			name: 'Github',
-			icon: 'github',
-			link: 'https://github.com/',
+			id: "1",
+			name: "Github",
+			icon: "github",
+			link: "https://github.com/",
 		},
 		{
-			id: '2',
-			name: 'Mail',
-			icon: 'mail',
-			link: 'https://mail.protonmail.com/',
+			id: "2",
+			name: "Mail",
+			icon: "mail",
+			link: "https://mail.protonmail.com/",
 		},
 		{
-			id: '3',
-			name: 'Todoist',
-			icon: 'trello',
-			link: 'https://todoist.com',
+			id: "3",
+			name: "Todoist",
+			icon: "trello",
+			link: "https://todoist.com",
 		},
 		{
-			id: '4',
-			name: 'Calendar',
-			icon: 'calendar',
-			link: 'https://calendar.google.com/calendar/r',
+			id: "4",
+			name: "Calendar",
+			icon: "calendar",
+			link: "https://calendar.google.com/calendar/r",
 		},
 		{
-			id: '5',
-			name: 'Reddit',
-			icon: 'glasses',
-			link: 'https://reddit.com',
+			id: "5",
+			name: "Reddit",
+			icon: "glasses",
+			link: "https://reddit.com",
 		},
 		{
-			id: '6',
-			name: 'Odysee',
-			icon: 'youtube',
-			link: 'https://odysee.com/',
+			id: "6",
+			name: "Odysee",
+			icon: "youtube",
+			link: "https://odysee.com/",
 		},
 	],
 
 	secondButtonsContainer: [
 		{
-			id: '1',
-			name: 'Music',
-			icon: 'headphones',
-			link: 'https://open.spotify.com',
+			id: "1",
+			name: "Music",
+			icon: "headphones",
+			link: "https://open.spotify.com",
 		},
 		{
-			id: '2',
-			name: 'twitter',
-			icon: 'twitter',
-			link: 'https://twitter.com/',
+			id: "2",
+			name: "twitter",
+			icon: "twitter",
+			link: "https://twitter.com/",
 		},
 		{
-			id: '3',
-			name: 'bot',
-			icon: 'bot',
-			link: 'https://discord.com/app',
+			id: "3",
+			name: "bot",
+			icon: "bot",
+			link: "https://discord.com/app",
 		},
 		{
-			id: '4',
-			name: 'Amazon',
-			icon: 'shopping-bag',
-			link: 'https://amazon.com/',
+			id: "4",
+			name: "Amazon",
+			icon: "shopping-bag",
+			link: "https://amazon.com/",
 		},
 		{
-			id: '5',
-			name: 'Hashnode',
-			icon: 'pen-tool',
-			link: 'https://hashnode.com/',
+			id: "5",
+			name: "Hashnode",
+			icon: "pen-tool",
+			link: "https://hashnode.com/",
 		},
 		{
-			id: '6',
-			name: 'Figma',
-			icon: 'figma',
-			link: 'https://figma.com/',
+			id: "6",
+			name: "Figma",
+			icon: "figma",
+			link: "https://figma.com/",
 		},
 	],
 
@@ -135,46 +135,46 @@ const CONFIG = {
 	// First Links Container
 	firstlistsContainer: [
 		{
-			icon: 'user',
-			id: '1',
+			icon: "user",
+			id: "1",
 			links: [
 				{
-					name: 'Youtube',
-					link: 'https://www.youtube.com/',
+					name: "Youtube",
+					link: "https://www.youtube.com/",
 				},
 				{
-					name: 'Reddit',
-					link: 'https://www.reddit.com/',
+					name: "Reddit",
+					link: "https://www.reddit.com/",
 				},
 				{
-					name: 'Twitter',
-					link: 'https://twitter.com/home',
+					name: "LastFM",
+					link: "https://www.last.fm/user/BastiGonzales",
 				},
 				{
-					name: 'Twitch',
-					link: 'https://www.twitch.tv/',
+					name: "Twitch",
+					link: "https://www.twitch.tv/",
 				},
 			],
 		},
 		{
-			icon: 'laptop',
-			id: '2',
+			icon: "laptop",
+			id: "2",
 			links: [
 				{
-					name: 'Linkedin',
-					link: 'https://www.linkedin.com',
+					name: "Linkedin",
+					link: "https://www.linkedin.com",
 				},
 				{
-					name: 'AMA',
-					link: 'https://discipulus.amasystem.net',
+					name: "AMA",
+					link: "https://discipulus.amasystem.net",
 				},
 				{
-					name: 'Stackoverflow',
-					link: 'https://stackoverflow.com/',
+					name: "Google Bard",
+					link: "https://bard.google.com/",
 				},
 				{
-					name: 'Cisco Netacad',
-					link: 'https://www.netacad.com/',
+					name: "Cisco Netacad",
+					link: "https://www.netacad.com/",
 				},
 			],
 		},
@@ -183,46 +183,46 @@ const CONFIG = {
 	// Second Links Container
 	secondListsContainer: [
 		{
-			icon: 'book-open',
-			id: '1',
+			icon: "book-open",
+			id: "1",
 			links: [
 				{
-					name: '1337x',
-					link: 'https://www.1337x.to/',
+					name: "1337x",
+					link: "https://www.1337x.to/",
 				},
 				{
-					name: 'Odin Project',
-					link: 'https://www.theodinproject.com/dashboard',
+					name: "Odin Project",
+					link: "https://www.theodinproject.com/dashboard",
 				},
 				{
-					name: 'freecodecamp',
-					link: 'https://www.freecodecamp.org/learn/2022/responsive-web-design',
+					name: "freecodecamp",
+					link: "https://www.freecodecamp.org/learn/2022/responsive-web-design",
 				},
 				{
-					name: 'Web Developer Roadmap',
-					link: 'https://roadmap.sh/',
+					name: "Web Developer Roadmap",
+					link: "https://roadmap.sh/",
 				},
 			],
 		},
 		{
-			icon: 'github',
-			id: '2',
+			icon: "github",
+			id: "2",
 			links: [
 				{
-					name: 'Profile',
-					link: 'https://github.com/Bastigonzales',
+					name: "Profile",
+					link: "https://github.com/Bastigonzales",
 				},
 				{
-					name: 'Stars',
-					link: 'https://github.com/Bastigonzales?tab=stars',
+					name: "Stars",
+					link: "https://github.com/Bastigonzales?tab=stars",
 				},
 				{
-					name: 'Explore',
-					link: 'https://github.com/explore',
+					name: "Explore",
+					link: "https://github.com/explore",
 				},
 				{
-					name: 'Repos',
-					link: 'https://github.com/Bastigonzales?tab=repositories',
+					name: "Repos",
+					link: "https://github.com/Bastigonzales?tab=repositories",
 				},
 			],
 		},
